793596d90a7f85e7c7249f90b3ede4c7bc3706a2
Alejandro Hita <alejandrohitagarrido@gmail.com>
2024-06-30 10:21:24 +0200

    First commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+	description = "My Neovim config flake";
+
+	inputs = {
+		nixpkgs = {
+			url = "github:NixOS/nixpkgs";
+		};
+		neovim = {
+			url = "github:neovim/neovim/stable?dir=contrib";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = { self, nixpkgs, neovim, ... }@inputs: {
+		packages.x86_64-linux.default = neovim.packages.x86_64-linux.neovim;
+      		apps.x86_64-linux.default = {
+        		type = "app";
+        		program = "${neovim.packages.x86_64-linux.neovim}/bin/nvim";
+      		};
+	};
+}
